4fd93b49c413300a1af15c14adf953f80c11bae8
Morow73 <39746007+Morow73@users.noreply.github.com>
2019-05-05 16:36:33 +0200

    Add Config

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.EnableHud = false -- enable/disable hud status bar
+
+-- decrease or increase exp gain
+
+Config.ExpTir       = 6000 -- shoot
+Config.ExpEndurance = 8000 -- stamina
+Config.ExpConduite  = 6000 -- drive
+Config.ExpForce     = 8000 -- strength
+Config.ExpNage      = 8000 -- swim 
+
+-- decrease or increase the speed of loss of exp
+
+Config.RemoveNage        = 150 -- swim
+Config.RemoveTir         = 150 -- shoot
+Config.RemoveConduite    = 150 -- drive
+Config.RemoveEndurance   = 150 -- stamina
+Config.RemoveForce       = 150 -- strength
