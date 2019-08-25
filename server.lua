f061530e3c23dbced79454d7cd7e47f06ef55397
Morow73 <39746007+Morow73@users.noreply.github.com>
2019-08-25 21:53:09 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,9 +3,9 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterUsableItem('coke_pooch', function(source)
-	local xPlayer  = ESX.GetPlayerFromId(source)
 	local _source  = source
-
+	local xPlayer  = ESX.GetPlayerFromId(source)
+	
 	TriggerClientEvent('esx_addonneeds:cokeeffect', _source)
 	xPlayer.removeInventoryItem('coke_pooch', 1)
 	TriggerClientEvent('esx:showNotification', _source, 'Tu à sniffer un peu de coke')
